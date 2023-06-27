ff0a5b9d2ab3fb8f335e22195f6fcf5117cf2398
Kynoskefalaj <91989136+Kynoskefalaj@users.noreply.github.com>
2023-06-27 20:27:56 +0200

    gameLoop delay 60Hz

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -12,12 +12,24 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth = tileSize * maxScreenCol; //768 px
     final int screenHeight = tileSize * maxScreenRow; //576 px
 
+//    FPS
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+//    Set player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 3;
+
 
     public GamePanel () {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true); //enabling this will improve performance
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -27,19 +39,80 @@ public class GamePanel extends JPanel implements Runnable{
     }
 
     @Override
-    public void run() {
+    public void run() { //SLEEP game loop
+
+        //          1000000000 nanoseconds == 1 second
+        double drawInterval = (double) 1000000000 / FPS; // 0.01666 seconds
+        double nextDrawTime = System.nanoTime() + drawInterval;
 
         while (gameThread != null) {
-            System.out.println("The game is running in a loop");
+//            System.out.println("The game is running in a loop");
 //            1 UPDATE: update information such as character position
             update();
 //            2 DRAW: draw the screen with the updated information
             repaint();
 
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime /= 1000000; //converts time from nano to miliseconds
+
+                if(remainingTime < 0) {
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long) remainingTime);
+
+                nextDrawTime += drawInterval;
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
         }
     }
+//    @Override
+//    public void run() { //DELTA / ACCUMULATOR game loop
+//
+//        double drawInterval = 1000000000 / FPS;
+//        double delta = 0;
+//        long lastTime = System.nanoTime();
+//        long currentTime;
+//        long timer = 0;
+//        int drawCount = 0;
+//
+//        while (gameThread != null) {
+//            currentTime = System.nanoTime();
+//            delta += (currentTime - lastTime) / drawInterval;
+//            timer += (currentTime - lastTime);
+//            lastTime = currentTime;
+//
+//            if (delta >= 1) {
+//                update();
+//                repaint();
+//                delta--;
+//                drawCount++;
+//            }
+//            if (timer >= 1000000000) {
+//                System.out.println("FPS: " + drawCount);
+//                drawCount = 0;
+//                timer = 0;
+//            }
+//        }
+//    }
 
     public void update() {
+
+//        don't have to write like "(keyH.upPressed == true)" in each statement because this field is boolean type,
+//        so statement is true if that field is true
+        if (keyH.upPressed) {
+            playerY -= playerSpeed;
+        } else if (keyH.downPressed) {
+            playerY += playerSpeed;
+        } else if (keyH.leftPressed) {
+            playerX -= playerSpeed;
+        } else if (keyH.rightPressed) {
+            playerX += playerSpeed;
+        }
 
     }
 
@@ -50,7 +123,7 @@ public class GamePanel extends JPanel implements Runnable{
         Graphics2D g2 = (Graphics2D)g;
 
         g2.setColor(Color.white);
-        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
         g2.dispose();
     }
 }
