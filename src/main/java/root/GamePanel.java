6494c95a9cf3b5d32d9fe1d72e1462f616d85564
Kynoskefalaj <91989136+Kynoskefalaj@users.noreply.github.com>
2023-08-06 23:18:35 +0200

    GameLoop change, UI draw bugfix

--- src/main/java/root/GamePanel.java
+++ src/main/java/root/GamePanel.java
@@ -110,67 +110,67 @@ public class GamePanel extends JPanel implements Runnable{
         gameThread.start();
     }
     @Override
-    public void run() { //SLEEP game loop
-
-        //          1000000000 nanoseconds == 1 second
-        double drawInterval = (double) 1000000000 / FPS; // 0.01666 seconds
-        double nextDrawTime = System.nanoTime() + drawInterval;
-
-        while (gameThread != null) {
-//            System.out.println("The game is running in a loop");
-//            1 UPDATE: update information such as character position
-            update();
-//            2 DRAW: draw the screen with the updated information
-            drawToTempScreen(); // Draw everything to the buffered image
-            drawToScreen(); // draw the buffered image to the screen
-
-            try {
-                double remainingTime = nextDrawTime - System.nanoTime();
-                remainingTime /= 1000000; //converts time from nano to miliseconds
-
-                if(remainingTime < 0) {
-                    remainingTime = 0;
-                }
-
-                Thread.sleep((long) remainingTime);
-
-                nextDrawTime += drawInterval;
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-        }
-    }
-//    @Override
-//    public void run() { //DELTA / ACCUMULATOR game loop
+//    public void run() { //SLEEP game loop
 //
-//        double drawInterval = 1000000000 / FPS;
-//        double delta = 0;
-//        long lastTime = System.nanoTime();
-//        long currentTime;
-//        long timer = 0;
-//        int drawCount = 0;
+//        //          1000000000 nanoseconds == 1 second
+//        double drawInterval = (double) 1000000000 / FPS; // 0.01666 seconds
+//        double nextDrawTime = System.nanoTime() + drawInterval;
 //
 //        while (gameThread != null) {
-//            currentTime = System.nanoTime();
-//            delta += (currentTime - lastTime) / drawInterval;
-//            timer += (currentTime - lastTime);
-//            lastTime = currentTime;
+////            System.out.println("The game is running in a loop");
+////            1 UPDATE: update information such as character position
+//            update();
+////            2 DRAW: draw the screen with the updated information
+//            drawToTempScreen(); // Draw everything to the buffered image
+//            drawToScreen(); // draw the buffered image to the screen
 //
-//            if (delta >= 1) {
-//                update();
-//                repaint();
-//                delta--;
-//                drawCount++;
+//            try {
+//                double remainingTime = nextDrawTime - System.nanoTime();
+//                remainingTime /= 1000000; //converts time from nano to miliseconds
+//
+//                if(remainingTime < 0) {
+//                    remainingTime = 0;
+//                }
+//
+//                Thread.sleep((long) remainingTime);
+//
+//                nextDrawTime += drawInterval;
+//
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
 //            }
-//            if (timer >= 1000000000) {
-//                System.out.println("FPS: " + drawCount);
-//                drawCount = 0;
-//                timer = 0;
-//            }
+//
 //        }
 //    }
+    public void run() { //DELTA / ACCUMULATOR game loop
+
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
+        while (gameThread != null) {
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                drawToTempScreen(); // Draw everything to the buffered image
+                drawToScreen(); // draw the buffered image to the screen
+                delta--;
+                drawCount++;
+            }
+            if (timer >= 1000000000) {
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+        }
+    }
     public void update() {
 //        don't have to write like "(keyH.upPressed == true)" in each statement because this field is boolean type,
 //        so statement is true if that field is true
@@ -257,9 +257,6 @@ public class GamePanel extends JPanel implements Runnable{
                 }
             }
 
-            // USER INTERFACE
-            ui.draw(g2);
-
             for (Entity obj : objects) {
                 if (obj != null) {
                     entityList.add(obj);
@@ -301,6 +298,9 @@ public class GamePanel extends JPanel implements Runnable{
             }
             // EMPTY ENTITY LIST (otherwise it will grow on every loop)
             entityList.clear();
+
+            // USER INTERFACE
+            ui.draw(g2);
 
             if (keyH.checkDrawTime == true) {
                 long drawEnd = System.nanoTime();
