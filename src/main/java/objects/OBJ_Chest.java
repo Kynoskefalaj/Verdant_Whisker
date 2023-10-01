dbb54ca1306931331571315cf9124cd70e6cb769
Kynoskefalaj <91989136+Kynoskefalaj@users.noreply.github.com>
2023-10-01 23:40:24 +0200

    Chest loot

--- src/main/java/objects/OBJ_Chest.java
+++ src/main/java/objects/OBJ_Chest.java
@@ -6,10 +6,53 @@ import root.GamePanel;
 
 public class OBJ_Chest extends Entity {
 
-    public OBJ_Chest (GamePanel gp) {
+    GamePanel gp;
+    Entity loot;
+    boolean opened = false;
+
+    public OBJ_Chest (GamePanel gp, Entity loot) {
         super(gp);
+        this.gp = gp;
+        this.loot = loot;
+
         name = "Chest";
-        down1 = setUp("/objects/chest01", gp.tileSize, gp.tileSize);
+        image1 = setUp("/objects/chest01", gp.tileSize, gp.tileSize);
+        image2 = setUp("objects/chest_opened", gp.tileSize, gp.tileSize);
+        down1 = image1;
+
         type = EntityType.OBSTACLE;
+        collision = true;
+
+        solidArea.x = 4;
+        solidArea.y = 16;
+        solidArea.width = 40;
+        solidArea.height = 32;
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
+    }
+
+    public void interact() {
+        gp.gameState = gp.dialogueState;
+
+        if(opened == false) {
+            gp.playSE(gp.se.unlockSE);
+
+            StringBuilder sb = new StringBuilder();
+            sb.append("You've opened the chest and find a " + loot.name + "!");
+
+            if (gp.player.inventory.size() == gp.player.maxInventorySize) {
+                sb.append("\n...But you cannot carry any more!");
+            }
+            else {
+                sb.append("\nYou have obtained the " + loot.name + "!");
+                gp.player.inventory.add(loot);
+                down1 = image2;
+                opened = true;
+            }
+            gp.ui.currentDialogue = sb.toString();
+        }
+        else {
+            gp.ui.currentDialogue = "It's empty.";
+        }
     }
 }
